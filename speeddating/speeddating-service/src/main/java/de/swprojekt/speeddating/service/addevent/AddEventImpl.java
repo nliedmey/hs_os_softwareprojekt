1b9f6edd8334df5b4dc15d691b582d4f86edc0ac
mariuss97 <marius.lange1997@gmail.com>
2019-11-28 19:09:23 +0100

    service addevent hinzugefuegt

---
+++ speeddating/speeddating-service/src/main/java/de/swprojekt/speeddating/service/addevent/AddEventImpl.java
@@ -0,0 +1,28 @@
+package de.swprojekt.speeddating.service.addevent;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import de.swprojekt.speeddating.model.Event;
+import de.swprojekt.speeddating.repository.IEventRepository;
+/*
+ * Implementierung von IAddEventService 
+ */
+@Service	//Service ist erweitertes @Component
+public class AddEventImpl implements IAddEventService {
+
+	@Autowired
+	IEventRepository iEventRepository;	//Interface zur Verwaltung von Events aus DB einbinden
+	
+	@Override
+	public void speicherEvent(Event einEventDAO) {
+		Event einEvent=new Event();	//neues Event-Objekt anlegen
+		einEvent.setBezeichnung(einEventDAO.getBezeichnung());
+		einEvent.setStartzeitpunkt(einEventDAO.getStartzeitpunkt());
+		einEvent.setEndzeitpunkt(einEventDAO.getEndzeitpunkt());
+		einEvent.setAbgeschlossen(einEventDAO.isAbgeschlossen());
+		einEvent.setTeilnehmendeStudierende(einEventDAO.getTeilnehmendeStudierende());
+		iEventRepository.save(einEvent);	//Speicherung in DB
+	}
+
+}
---
+++ speeddating/speeddating-service/src/main/java/de/swprojekt/speeddating/service/addevent/IAddEventService.java
@@ -0,0 +1,7 @@
+package de.swprojekt.speeddating.service.addevent;
+
+import de.swprojekt.speeddating.model.Event;
+
+public interface IAddEventService {
+	public void speicherEvent(Event einEvent);
+}
