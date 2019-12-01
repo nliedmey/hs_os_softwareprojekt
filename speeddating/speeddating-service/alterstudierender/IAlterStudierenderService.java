194d84a64a332907a97aa8f5d40d40f99b952a0b
mariuss97 <marius.lange1997@gmail.com>
2019-12-01 23:47:29 +0100

    falsche Ordner commitet

--- speeddating/speeddating-service/alterstudierender/IAlterStudierenderService.java
+++
@@ -1,7 +0,0 @@
-package de.swprojekt.speeddating.service.alterstudierender;
-
-import de.swprojekt.speeddating.model.Studierender;
-
-public interface IAlterStudierenderService {
-	public Studierender aenderStudierenden(Studierender veraenderterStudierenderDAO);
-}
