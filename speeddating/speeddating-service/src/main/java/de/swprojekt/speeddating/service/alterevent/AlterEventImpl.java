6d71f8c723ceaf15b7ebe55942abf5c4ceb3af9d
mariuss97 <marius.lange1997@gmail.com>
2019-12-01 14:30:19 +0100

    Services fuer Aendern von Event hinzugefuegt

---
+++ speeddating/speeddating-service/src/main/java/de/swprojekt/speeddating/service/alterevent/AlterEventImpl.java
@@ -0,0 +1,39 @@
+package de.swprojekt.speeddating.service.alterevent;
+
+import java.util.NoSuchElementException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import de.swprojekt.speeddating.model.Event;
+import de.swprojekt.speeddating.repository.IEventRepository;
+
+/*
+ * Implementierung des IAlterEventService-Interfaces
+ * Eingebunden in Views wird hingegen nur das Interface! 
+ * (hierfuer muss die Implementierung als Service deklariert sein!)
+ */
+@Service
+public class AlterEventImpl implements IAlterEventService {
+
+	@Autowired // Repository wird autowired
+	IEventRepository iEventRepository; // Zugriff auf Studierende-Entities in DB
+
+	@Override
+	public Event aenderEvent(Event veraendertesEventDAO) {
+		try {
+			Event gefundenesEvent = iEventRepository
+					.findById(veraendertesEventDAO.getEvent_id()).get(); // existierendes Eventobjekt aus DB holen
+			gefundenesEvent.setBezeichnung(veraendertesEventDAO.getBezeichnung());
+			gefundenesEvent.setStartzeitpunkt(veraendertesEventDAO.getStartzeitpunkt());
+			gefundenesEvent.setEndzeitpunkt(veraendertesEventDAO.getEndzeitpunkt());
+			gefundenesEvent.setAbgeschlossen(veraendertesEventDAO.isAbgeschlossen());
+			gefundenesEvent.setTeilnehmendeStudierende(veraendertesEventDAO.getTeilnehmendeStudierende());
+			return iEventRepository.save(gefundenesEvent);
+		} catch (NoSuchElementException e) {	//wenn findById() kein Ergebnis liefert
+			System.out.println("Kein Event zu ID vorhanden!");
+			return null;
+		}
+	}
+
+}
---
+++ speeddating/speeddating-service/src/main/java/de/swprojekt/speeddating/service/alterevent/IAlterEventService.java
@@ -0,0 +1,7 @@
+package de.swprojekt.speeddating.service.alterevent;
+
+import de.swprojekt.speeddating.model.Event;
+
+public interface IAlterEventService {
+	public Event aenderEvent(Event veraendertesEventDAO);
+}
