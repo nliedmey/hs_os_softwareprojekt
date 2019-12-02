8141f4bec7fc3d9144f4e81307b0ee73694d110b
patri <patri@192.168.0.8>
2019-12-02 22:52:18 +0100

    Anp. für Kontaktwünsche View

--- speeddating/speeddating-ui/src/main/java/de/swprojekt/speeddating/ui/EventView.java
+++ speeddating/speeddating-ui/src/main/java/de/swprojekt/speeddating/ui/EventView.java
@@ -22,7 +22,7 @@ import de.swprojekt.speeddating.service.showevent.IShowEventService;
  * View fuer die Anzeige vorhandener Events
  */
 
-@Route("ui/eventVotingView_Stud")	//Erreichbar ueber Adresse: http://localhost:8080/speeddating-web-7.0-SNAPSHOT/ui/events
+@Route("ui/event")	//Erreichbar ueber Adresse: http://localhost:8080/speeddating-web-7.0-SNAPSHOT/ui/events
 //@Secured("ROLE_ADMIN")	//nur User mit Rolle ADMIN koennen auf Seite zugreifen, @Secured prueft auch bei RouterLink-Weiterleitungen
 //@Secured kann auch an einzelnen Methoden angewendet werden
 public class EventView extends VerticalLayout {	//VerticalLayout fuehrt zu Anordnung von Elementen untereinander statt nebeneinander (HorizontalLayout)
@@ -30,27 +30,27 @@ public class EventView extends VerticalLayout {	//VerticalLayout fuehrt zu Anord
 	@Autowired	//Konstruktor-basierte Injection, Parameter wird autowired (hier: Interface)
 	public EventView(IShowEventService iShowEventService, IDeleteEventService iDeleteEventService) {
 	
-		Grid<Unternehmen> teilgenommendeUnternehmenGrid;	//Tabelle mit Events
-		GridMultiSelectionModel<Unternehmen> selectionModelEvent;
-		Button buttonVotingAbschicken =new Button("Voting abschicken");
+		Grid<Event> eventGrid;	//Tabelle mit Events
+		GridMultiSelectionModel<Event> selectionModelEvent;
+		Button loeschenButton=new Button("Loeschen");		
 		Button logoutButton=new Button("Logout");
 		
-//	teilgenommendeUnternehmenGrid = new Grid<>(Unternehmen.class);	//Tabelle initialisieren
-//		ListDataProvider<Unternehmen> ldpEvent = DataProvider
-//				.ofCollection(iShowEventService.showEvents());	//Dataprovider erstellen und Quelle fuer Events (via Service aus DB) festlegen 
-//		eventViewGrid.setDataProvider(ldpEvent);	//erstellten Dataprovider als Datenquelle fuer Tabelle festlegen
+		eventGrid = new Grid<>(Event.class);	//Tabelle initialisieren
+		ListDataProvider<Event> ldpEvent = DataProvider
+				.ofCollection(iShowEventService.showEvents());	//Dataprovider erstellen und Quelle fuer Events (via Service aus DB) festlegen 
+		eventGrid.setDataProvider(ldpEvent);	//erstellten Dataprovider als Datenquelle fuer Tabelle festlegen
 		
-//		eventViewGrid.removeColumnByKey("event_id");	//event_id nicht in Tabelle mit anzeigen
-//		eventViewGrid.setColumns("unternehmensname");	//Spaltenordnung festlegen
-//		eventViewGrid.setSelectionMode(SelectionMode.MULTI);	//es koennen mehrere Events ausgewaehlt sein
-//		selectionModelEvent = (GridMultiSelectionModel<Unternehmen>) eventViewGrid.getSelectionModel();
-			
-//		buttonVotingAbschicken.addClickListener(event -> {	//Bei Buttonklick werden folgende Aktionen ausgefuehrt
-//			for(Event e:selectionModelEvent.getSelectedItems())	//markierte Events durchgehen
-//			{
-//				iDeleteEventService.loescheEvent(e);
-//			}
-//		});
+		eventGrid.removeColumnByKey("event_id");	//event_id nicht in Tabelle mit anzeigen
+		eventGrid.setColumns("bezeichnung", "startzeitpunkt", "endzeitpunkt", "abgeschlossen", "teilnehmendeStudierende");	//Spaltenordnung festlegen
+		eventGrid.setSelectionMode(SelectionMode.MULTI);	//es koennen mehrere Events ausgewaehlt sein
+		selectionModelEvent = (GridMultiSelectionModel<Event>) eventGrid.getSelectionModel();
+		
+		loeschenButton.addClickListener(event -> {	//Bei Buttonklick werden folgende Aktionen ausgefuehrt
+			for(Event e:selectionModelEvent.getSelectedItems())	//markierte Events durchgehen
+			{
+				iDeleteEventService.loescheEvent(e);
+			}
+		});
 		
 		logoutButton.addClickListener(event -> {	//Bei Buttonklick werden folgende Aktionen ausgefuehrt
 			SecurityContextHolder.clearContext();	//Spring-Security-Session leeren
@@ -59,8 +59,8 @@ public class EventView extends VerticalLayout {	//VerticalLayout fuehrt zu Anord
 		});
 
 
-//		add(eventViewGrid);	//Hinzufuegen der Elemente zum VerticalLayout
-		add(buttonVotingAbschicken);
+		add(eventGrid);	//Hinzufuegen der Elemente zum VerticalLayout
+		add(loeschenButton);
 		add(logoutButton);
 	}
 	//@PostConstruct	//Ausfuehrung nach Konstruktoraufruf
--- speeddating/speeddating-ui/src/main/java/de/swprojekt/speeddating/ui/EventVotingView_Stud.java
+++ speeddating/speeddating-ui/src/main/java/de/swprojekt/speeddating/ui/EventVotingView_Stud.java
@@ -1,9 +1,13 @@
 package de.swprojekt.speeddating.ui;
 
+
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.GridMultiSelectionModel;
 import com.vaadin.flow.component.grid.Grid.SelectionMode;
+
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
@@ -15,42 +19,72 @@ import com.vaadin.flow.data.provider.ListDataProvider;
 import com.vaadin.flow.router.Route;
 
 import de.swprojekt.speeddating.model.Event;
+import de.swprojekt.speeddating.model.Studierender;
+import de.swprojekt.speeddating.model.Unternehmen;
 import de.swprojekt.speeddating.service.deleteevent.IDeleteEventService;
 import de.swprojekt.speeddating.service.showevent.IShowEventService;
+import de.swprojekt.speeddating.service.showunternehmen.IShowUnternehmenService;
+import de.swprojekt.speeddating.service.unternehmen.IUnternehmenService;
 /*
  * View fuer die Anzeige vorhandener Events
  */  
 
-@Route("ui/events")	//Erreichbar ueber Adresse: http://localhost:8080/speeddating-web-7.0-SNAPSHOT/ui/events
+@Route("ui/eventVotingView_Stud")	//Erreichbar ueber Adresse: http://localhost:8080/speeddating-web-7.0-SNAPSHOT/ui/events
 //@Secured("ROLE_ADMIN")	//nur User mit Rolle ADMIN koennen auf Seite zugreifen, @Secured prueft auch bei RouterLink-Weiterleitungen
 //@Secured kann auch an einzelnen Methoden angewendet werden
 public class EventVotingView_Stud extends VerticalLayout {	//VerticalLayout fuehrt zu Anordnung von Elementen untereinander statt nebeneinander (HorizontalLayout)
 
 	@Autowired	//Konstruktor-basierte Injection, Parameter wird autowired (hier: Interface)
-	public EventVotingView_Stud(IShowEventService iShowEventService, IDeleteEventService iDeleteEventService) {
+	public EventVotingView_Stud(IShowEventService iShowEventService, IShowUnternehmenService iShowUnternehmenService) {
 	
-		Grid<Event> eventGrid;	//Tabelle mit Events
-		GridMultiSelectionModel<Event> selectionModelEvent;
-		Button loeschenButton=new Button("Loeschen");
-		
+		Grid<Unternehmen> unternehmenGrid;	//Tabelle mit Events
+		unternehmenGrid = new Grid<>(Unternehmen.class);	//Tabelle initialisieren
+		GridMultiSelectionModel<Unternehmen> selectionModelUnternehmen;
+		Button votingSendenButton=new Button("Kontaktwuensche absenden");		
 		Button logoutButton=new Button("Logout");
 		
-		eventGrid = new Grid<>(Event.class);	//Tabelle initialisieren
-		ListDataProvider<Event> ldpEvent = DataProvider
-				.ofCollection(iShowEventService.showEvents());	//Dataprovider erstellen und Quelle fuer Events (via Service aus DB) festlegen 
-		eventGrid.setDataProvider(ldpEvent);	//erstellten Dataprovider als Datenquelle fuer Tabelle festlegen
 		
-		eventGrid.removeColumnByKey("event_id");	//event_id nicht in Tabelle mit anzeigen
-		eventGrid.setColumns("bezeichnung", "startzeitpunkt", "endzeitpunkt", "abgeschlossen", "teilnehmendeStudierende");	//Spaltenordnung festlegen
-		eventGrid.setSelectionMode(SelectionMode.MULTI);	//es koennen mehrere Events ausgewaehlt sein
-		selectionModelEvent = (GridMultiSelectionModel<Event>) eventGrid.getSelectionModel();
-		
-		loeschenButton.addClickListener(event -> {	//Bei Buttonklick werden folgende Aktionen ausgefuehrt
-			for(Event e:selectionModelEvent.getSelectedItems())	//markierte Events durchgehen
-			{
-				iDeleteEventService.loescheEvent(e);
+		// Erzeugen der Combo Box
+		ComboBox<Event> comboBox = new ComboBox<>();
+		comboBox.setLabel("Event auswaehlen");
+		comboBox.setItemLabelGenerator(Event::getBezeichnung);
+		List<Event> listOfEvents = iShowEventService.showEvents();
+		List<Unternehmen> listOfUnternehmenForDisplay = null;
+
+		comboBox.setItems(listOfEvents);
+		comboBox.addValueChangeListener(event -> {
+			Event aEvent = comboBox.getValue();
+			
+			if (aEvent != null) {				
+				List<Unternehmen> listofUnternehmen = iShowUnternehmenService.showUnternehmen(); //alle Unternehmen holen					
+				for( Integer unternehmenDesEvents : aEvent.getTeilnehmendeUnternehmen()) { 
+					for( Unternehmen aUnternehmen: listofUnternehmen) {						
+						if (aUnternehmen.getUnternehmen_id() == unternehmenDesEvents) {							
+							listOfUnternehmenForDisplay.add(aUnternehmen);
+						}						
+					}					
+				}
+				
+				ListDataProvider<Unternehmen> ldpEvent = DataProvider
+						.ofCollection(listOfUnternehmenForDisplay);			
+				unternehmenGrid.setDataProvider(ldpEvent);	//erstellten Dataprovider als Datenquelle fuer Tabelle festlegen
+			} else {
+				// message.setText("No song is selected");
 			}
+
 		});
+					
+		unternehmenGrid.removeColumnByKey("unternehmen_id");	//event_id nicht in Tabelle mit anzeigen
+		unternehmenGrid.setColumns("unternehmensname", "ansprechpartner", "kontaktmail");	//Spaltenordnung festlegen
+		unternehmenGrid.setSelectionMode(SelectionMode.MULTI);	//es koennen mehrere Events ausgewaehlt sein
+		selectionModelUnternehmen = (GridMultiSelectionModel<Unternehmen>) unternehmenGrid.getSelectionModel();
+		
+//		VotingSendenButton.addClickListener(event -> {	//Bei Buttonklick werden folgende Aktionen ausgefuehrt
+//			for(Event e:selectionModelEvent.getSelectedItems())	//markierte Events durchgehen
+//			{
+//				iDeleteEventService.loescheEvent(e);
+//			}
+//		});
 		
 		logoutButton.addClickListener(event -> {	//Bei Buttonklick werden folgende Aktionen ausgefuehrt
 			SecurityContextHolder.clearContext();	//Spring-Security-Session leeren
@@ -58,9 +92,9 @@ public class EventVotingView_Stud extends VerticalLayout {	//VerticalLayout fueh
 			logoutButton.getUI().ifPresent(ui->ui.navigate("maincontent"));	//zurueck auf andere Seite 
 		});
 
-
-		add(eventGrid);	//Hinzufuegen der Elemente zum VerticalLayout
-		add(loeschenButton);
+		add(comboBox);
+		add(unternehmenGrid);	//Hinzufuegen der Elemente zum VerticalLayout
+		add(votingSendenButton);
 		add(logoutButton);
 	}
 	//@PostConstruct	//Ausfuehrung nach Konstruktoraufruf
