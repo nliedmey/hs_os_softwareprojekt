2bc6c68c60fe06b1049a31dcc5bd09b7b0b64394
mariuss97 <marius.lange1997@gmail.com>
2019-12-09 22:19:11 +0100

    Uebergangsloesung fuer Adminerstellung via View (Link: /admininit)

---
+++ speeddating/speeddating-ui/src/main/java/de/swprojekt/speeddating/ui/AdminInit.java
@@ -0,0 +1,52 @@
+package de.swprojekt.speeddating.ui;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Label;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+import de.swprojekt.speeddating.model.Role;
+import de.swprojekt.speeddating.model.User;
+import de.swprojekt.speeddating.repository.IRoleRepository;
+import de.swprojekt.speeddating.repository.IUserRepository;
+
+/*
+ * Temporaere Loesung zum Erstellen von ADMIN!!!SICHERHEITSRISIKO!!
+ */
+@Route(value = "admininit", layout = MainLayout.class) // Abgeleitet von Root-Layout MainLayout
+public class AdminInit extends VerticalLayout {
+
+
+
+	@Autowired
+	public AdminInit(IRoleRepository iRoleRepository, IUserRepository iUserRepository, BCryptPasswordEncoder passwordEncoder) {
+
+		Button buttonAdminerstellen=new Button("ADMIN ERSTELLEN");
+		Label labelPWUsername=new Label("Name: admin, pw: admin, kann Menue ueber /ui/admin/menue erreichen und von dort eventorgas etc. erstellen");
+		
+		User einUser=new User();
+		einUser.setPassword(passwordEncoder.encode("admin"));
+		einUser.setUsername("admin");
+		Set<Role> roles=new HashSet<Role>();
+		roles.add(iRoleRepository.findByRolename("ADMIN"));
+		einUser.setRoles(roles);
+		iUserRepository.save(einUser);
+		
+		
+		
+		
+		buttonAdminerstellen.addClickListener(event -> {
+			System.out.println("ADMIN ERSTELLT!");
+		});
+		add(labelPWUsername);
+		add(buttonAdminerstellen);
+		
+
+	}
+
+}
