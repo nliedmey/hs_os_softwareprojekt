37273e39828e1489e7b76700e007340a1ef5aa05
mariu <mariu@DESKTOP-0RP8UP0>
2019-12-09 18:59:05 +0100

    Aendern Eventorganisator View angepasst

--- speeddating/speeddating-ui/src/main/java/de/swprojekt/speeddating/ui/AddEventorganisator.java
+++ speeddating/speeddating-ui/src/main/java/de/swprojekt/speeddating/ui/AddEventorganisator.java
@@ -2,44 +2,20 @@ package de.swprojekt.speeddating.ui;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
-import com.vaadin.flow.component.datepicker.DatePicker;
-import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.grid.Grid.SelectionMode;
-import com.vaadin.flow.component.grid.GridMultiSelectionModel;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.notification.NotificationVariant;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.ZoneId;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
-
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.component.timepicker.TimePicker;
 import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.data.binder.ValidationException;
-import com.vaadin.flow.data.converter.StringToIntegerConverter;
-import com.vaadin.flow.data.provider.DataProvider;
-import com.vaadin.flow.data.provider.ListDataProvider;
 import com.vaadin.flow.router.Route;
 
-import de.swprojekt.speeddating.model.Event;
 import de.swprojekt.speeddating.model.Eventorganisator;
-import de.swprojekt.speeddating.model.Studierender;
-import de.swprojekt.speeddating.model.Unternehmen;
-import de.swprojekt.speeddating.service.addevent.IAddEventService;
 import de.swprojekt.speeddating.service.addeventorganisator.IAddEventorganisatorService;
-import de.swprojekt.speeddating.service.showevent.IShowEventService;
-import de.swprojekt.speeddating.service.showstudierender.IShowStudierendeService;
-import de.swprojekt.speeddating.service.showunternehmen.IShowUnternehmenService;
 
 /*
  * View zum Hinzufuegen von Eventorganisator
