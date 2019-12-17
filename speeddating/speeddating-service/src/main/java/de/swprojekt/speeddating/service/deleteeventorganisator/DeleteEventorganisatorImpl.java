9cafabec5dd0d53580a98fab846281c1a7132821
patri <patri@192.168.0.5>
2019-12-17 22:18:54 +0100

    Loeschen des Benutzers wenn Eventorganisator gelöscht wird

--- speeddating/speeddating-service/src/main/java/de/swprojekt/speeddating/service/deleteeventorganisator/DeleteEventorganisatorImpl.java
+++ speeddating/speeddating-service/src/main/java/de/swprojekt/speeddating/service/deleteeventorganisator/DeleteEventorganisatorImpl.java
@@ -5,8 +5,11 @@ import org.springframework.stereotype.Service;
 
 import de.swprojekt.speeddating.model.Event;
 import de.swprojekt.speeddating.model.Eventorganisator;
+import de.swprojekt.speeddating.model.Studierender;
+import de.swprojekt.speeddating.model.User;
 import de.swprojekt.speeddating.repository.IEventRepository;
 import de.swprojekt.speeddating.repository.IEventorganisatorRepository;
+import de.swprojekt.speeddating.repository.IUserRepository;
 /*
  * Implementierung von IDeleteEventorganisatorService 
  */
@@ -16,9 +19,23 @@ public class DeleteEventorganisatorImpl implements IDeleteEventorganisatorServic
 	@Autowired
 	IEventorganisatorRepository iEventorganisatorRepository;	//Interface zur Verwaltung von Eventorganisatoren aus DB einbinden
 
+	@Autowired
+	IUserRepository iUserRepository;
+	
 	@Override
-	public void loescheEventorganisator(Eventorganisator einEventorganisator) {
-		iEventorganisatorRepository.delete(einEventorganisator);
+	public void loescheEventorganisator(Eventorganisator einEventorganisatorDAO) {
+		
+		Eventorganisator einEventOrganisator=new Eventorganisator();		
+		einEventOrganisator.setEventorganisator_id(einEventorganisatorDAO.getEventorganisator_id());
+		einEventOrganisator.setNachname(einEventorganisatorDAO.getNachname());
+
+		String username = einEventOrganisator.getNachname() + "_" + einEventOrganisator.getEventorganisator_id();
+		User gefundenerUser=iUserRepository.findByUsername(username);
+		if (gefundenerUser!= null) {
+			iUserRepository.delete(gefundenerUser);
+		}
+		
+		iEventorganisatorRepository.delete(einEventorganisatorDAO);
 	}
 
 }
--- speeddating/speeddating-ui/src/main/java/de/swprojekt/speeddating/ui/EventorganisatorView.java
+++ speeddating/speeddating-ui/src/main/java/de/swprojekt/speeddating/ui/EventorganisatorView.java
@@ -1,6 +1,7 @@
 package de.swprojekt.speeddating.ui;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.GridMultiSelectionModel;
 import com.vaadin.flow.component.html.Label;
@@ -14,6 +15,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.data.binder.ValidationException;
 import com.vaadin.flow.data.provider.DataProvider;
 import com.vaadin.flow.data.provider.ListDataProvider;
 import com.vaadin.flow.router.Route;
@@ -25,68 +27,87 @@ import de.swprojekt.speeddating.service.deleteeventorganisator.IDeleteEventorgan
  */
 import de.swprojekt.speeddating.service.showeventorganisator.IShowEventorganisatorService;
 
-@Route("ui/eventorganisator")	//Erreichbar ueber Adresse: http://localhost:8080/speeddating-web-7.0-SNAPSHOT/ui/eventorganisator
-@Secured("ROLE_ADMIN")	//nur User mit Rolle ADMIN koennen auf Seite zugreifen, @Secured prueft auch bei RouterLink-Weiterleitungen
+@Route("ui/eventorganisator") // Erreichbar ueber Adresse:
+								// http://localhost:8080/speeddating-web-7.0-SNAPSHOT/ui/eventorganisator
+@Secured("ROLE_ADMIN") // nur User mit Rolle ADMIN koennen auf Seite zugreifen, @Secured prueft auch
+						// bei RouterLink-Weiterleitungen
 //@Secured kann auch an einzelnen Methoden angewendet werden
-public class EventorganisatorView extends VerticalLayout {	//VerticalLayout fuehrt zu Anordnung von Elementen untereinander statt nebeneinander (HorizontalLayout)
+public class EventorganisatorView extends VerticalLayout { // VerticalLayout fuehrt zu Anordnung von Elementen
+															// untereinander statt nebeneinander (HorizontalLayout)
 
-	@Autowired	//Konstruktor-basierte Injection, Parameter wird autowired (hier: Interface)
-	public EventorganisatorView(IShowEventorganisatorService iShowEventorganisatorService, IDeleteEventorganisatorService iDeleteEventorganisatorService) {
-	
+	@Autowired // Konstruktor-basierte Injection, Parameter wird autowired (hier: Interface)
+	public EventorganisatorView(IShowEventorganisatorService iShowEventorganisatorService,
+			IDeleteEventorganisatorService iDeleteEventorganisatorService) {
+
 		Grid<Eventorganisator> eventorganisatorGrid; // Tabelle mit Eventorganisatoren
 		GridMultiSelectionModel<Eventorganisator> selectionModelEventorganisator;
-		Button loeschenButton=new Button("Loeschen");		
-		Button logoutButton=new Button("Logout");
+		Button loeschenButton = new Button("Loeschen");
+		Button logoutButton = new Button("Logout");
 		Button zurueckButton = new Button("Zurueck");
-		
-		
+
+		// Bestaetigungs-Popup
+		Button buttonBestaetigenJa = new Button("Ja");
+		Button buttonBestaetigenNein = new Button("Nein");
+		Dialog popUpBestaetigen = new Dialog();
+		popUpBestaetigen.add(new Label("Den Eventorganisator endgueltig loeschen?"));
+		popUpBestaetigen.add(buttonBestaetigenJa, buttonBestaetigenNein);
+
 		Notification notificationSavesuccess = new Notification();
 		notificationSavesuccess.addThemeVariants(NotificationVariant.LUMO_SUCCESS);
 		Label labelSavesuccess = new Label("Eventorganisator erfolgreich geloescht! ");
 		notificationSavesuccess.add(labelSavesuccess);
-		notificationSavesuccess.setDuration(2500); //Meldung wird 2,5 Sekunden lang angezeigt
-		
+		notificationSavesuccess.setDuration(2500); // Meldung wird 2,5 Sekunden lang angezeigt
+
 		eventorganisatorGrid = new Grid<>(Eventorganisator.class); // Tabelle initialisieren
 		ListDataProvider<Eventorganisator> ldpEventorganisator = DataProvider
-				.ofCollection(iShowEventorganisatorService.showEventorganisatoren()); // Dataprovider erstellen und Quelle fuer
-																			// Eventorganisatoren (via Service aus DB)
-																			// festlegen
-		eventorganisatorGrid.setDataProvider(ldpEventorganisator); // erstellten Dataprovider als Datenquelle fuer Tabelle festlegen
+				.ofCollection(iShowEventorganisatorService.showEventorganisatoren()); // Dataprovider erstellen und
+																						// Quelle fuer
+		// Eventorganisatoren (via Service aus DB)
+		// festlegen
+		eventorganisatorGrid.setDataProvider(ldpEventorganisator); // erstellten Dataprovider als Datenquelle fuer
+																	// Tabelle festlegen
 
-		eventorganisatorGrid.removeColumnByKey("eventorganisator_id");	//event_id nicht in Tabelle mit anzeigen
-		eventorganisatorGrid.setColumns("vorname", "nachname", "fachbereich", "telefonnr", "email","verwaltet_events");	//Spaltenordnung festlegen
-		eventorganisatorGrid.setSelectionMode(SelectionMode.MULTI);	//es koennen mehrere Organisatoren gleichzeitig geloescht werden
-		selectionModelEventorganisator = (GridMultiSelectionModel<Eventorganisator>) eventorganisatorGrid.getSelectionModel();
-		
-		loeschenButton.addClickListener(event -> {	//Bei Buttonklick werden folgende Aktionen ausgefuehrt
-			for(Eventorganisator eo:selectionModelEventorganisator.getSelectedItems())	//markierte Events durchgehen
-			{
+		eventorganisatorGrid.removeColumnByKey("eventorganisator_id"); // event_id nicht in Tabelle mit anzeigen
+		eventorganisatorGrid.setColumns("vorname", "nachname", "fachbereich", "telefonnr", "email", "verwaltet_events"); // Spaltenordnung
+																															// festlegen
+		eventorganisatorGrid.setSelectionMode(SelectionMode.MULTI); // es koennen mehrere Organisatoren gleichzeitig
+																	// geloescht werden
+		selectionModelEventorganisator = (GridMultiSelectionModel<Eventorganisator>) eventorganisatorGrid
+				.getSelectionModel();
+
+		loeschenButton.addClickListener(event -> { // Bei Buttonklick werden folgende Aktionen ausgefuehrt
+
+			popUpBestaetigen.open();
+
+		});
+
+		buttonBestaetigenJa.addClickListener(event -> {
+			for (Eventorganisator eo : selectionModelEventorganisator.getSelectedItems()){
 				iDeleteEventorganisatorService.loescheEventorganisator(eo);
 			}
-			
 			notificationSavesuccess.open(); // Erfolgreich-Meldung anzeigen
-			loeschenButton.getUI().ifPresent(ui->ui.navigate("ui/admin/menue"));	//zurueck auf andere Seite 
-		});
-		
-		logoutButton.addClickListener(event -> {	//Bei Buttonklick werden folgende Aktionen ausgefuehrt
-			SecurityContextHolder.clearContext();	//Spring-Security-Session leeren
-			getUI().get().getSession().close();		//Vaadin Session leeren
-			logoutButton.getUI().ifPresent(ui->ui.navigate("maincontent"));	//zurueck auf andere Seite 
-		});
-		
-		zurueckButton.addClickListener(event -> {	//Bei Buttonklick werden folgende Aktionen ausgefuehrt
-			logoutButton.getUI().ifPresent(ui->ui.navigate("ui/admin/menue"));	//zurueck auf andere Seite 
+			popUpBestaetigen.close();
+			loeschenButton.getUI().ifPresent(ui -> ui.navigate("ui/admin/menue")); // zurueck auf andere Seite
 		});
 
+		buttonBestaetigenNein.addClickListener(event -> {
+			popUpBestaetigen.close();
+		});
 
-		add(eventorganisatorGrid);	//Hinzufuegen der Elemente zum VerticalLayout
+		logoutButton.addClickListener(event -> { // Bei Buttonklick werden folgende Aktionen ausgefuehrt
+			SecurityContextHolder.clearContext(); // Spring-Security-Session leeren
+			getUI().get().getSession().close(); // Vaadin Session leeren
+			logoutButton.getUI().ifPresent(ui -> ui.navigate("login")); // zurueck auf andere Seite
+		});
+
+		zurueckButton.addClickListener(event -> { // Bei Buttonklick werden folgende Aktionen ausgefuehrt
+			logoutButton.getUI().ifPresent(ui -> ui.navigate("ui/admin/menue")); // zurueck auf andere Seite
+		});
+
+		add(eventorganisatorGrid); // Hinzufuegen der Elemente zum VerticalLayout
 		add(loeschenButton);
-		add(new HorizontalLayout(zurueckButton,logoutButton));
+		add(new HorizontalLayout(zurueckButton, logoutButton));
 
 	}
-	//@PostConstruct	//Ausfuehrung nach Konstruktoraufruf
-	//public void init()
-	//{
-	//	
-	//}
+
 }
