b099406de9ff4523690833287d4602637e6f1a02
mariuss97 <marius.lange1997@gmail.com>
2020-01-14 12:12:39 +0100

    EventDurchfuehrung Sounddatei lokal hinterlegt

--- speeddating/speeddating-ui/src/main/java/de/swprojekt/speeddating/ui/event/EventDurchfuehrung.java
+++ speeddating/speeddating-ui/src/main/java/de/swprojekt/speeddating/ui/event/EventDurchfuehrung.java
@@ -229,7 +229,11 @@ public class EventDurchfuehrung extends HorizontalLayout {
 //			String filepath = directory+"\\otherFiles\\"+filename;
 //			URL link = this.getClass().getResource(filepath);
 			AudioPlayer player=new AudioPlayer();
-			player.setSource("http://soundbible.com/mp3/Air Horn-SoundBible.com-964603082.mp3");
+			String directory=System.getProperty("jboss.server.data.dir"); //Property verweist auf Datenverzeichnes des Wildflyservers (auf Server: opt/wildfly)
+			String filename="zapsplat_sport_air_horn_1x_blast_009_21146.mp3";
+			String filepath = directory+"/otherData/"+filename;
+			player.setSource(filepath);
+//			player.setSource("http://soundbible.com/mp3/Air Horn-SoundBible.com-964603082.mp3");
 			
 //			player.play();
 //			URL link = this.getClass().getResource("./WAV001.WAV");
